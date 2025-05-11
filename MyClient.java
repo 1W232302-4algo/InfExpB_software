d2493ea3bfc218f9310a47e02cc21a587dd1ef78
1W232302-4algo <the.for.at.reality2@gmail.com>
2025-05-11 09:16:40 +0900

    Update MyClient.java

--- MyClient.java
+++ MyClient.java
@@ -15,7 +15,7 @@ import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
 public class MyClient {
-    public static int N = 11;
+    public static int N = 10;
 
     public static void main(String[] args) {
         final String SERVER_IP = "localhost";
@@ -66,21 +66,28 @@ public class MyClient {
     }
 
     private static class MyFrame extends JFrame {
-        private final JLabel[][] cells = new JLabel[N][N];
+        private final JLabel[][] cells = new JLabel[N][N + 1]; //edit
         private final Set<String> pressedKeys = new HashSet<>();
 
         public MyFrame() {
             setTitle("Game");
-            setSize(300, 300);
+            setSize(400, 400);
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            setLayout(new GridLayout(N, N));
+            setLayout(new GridLayout(N, N + 1)); //edited
 
             for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
+                for (int j = 0; j < N + 1; j++) {//edited
                     cells[i][j] = new JLabel("0", SwingConstants.CENTER);
                     add(cells[i][j]);
+
+                    cells[i][j].setOpaque(true);
+                    if (j == N) {
+                        cells[i][j].setBackground(java.awt.Color.LIGHT_GRAY);
+                    }
                 }
             }
+
+
 
             setVisible(true);
 
@@ -146,7 +153,38 @@ public class MyClient {
                 for (int j = 0; j < board.length; j++) {
                     board[i][j] = data[i * board.length + j];
                     cells[i][j].setText(board[i][j]);
+                    switch(Integer.parseInt(board[i][j])){
+                        case 21:
+                            cells[i][j].setForeground(java.awt.Color.GRAY);
+                            cells[i][j].setBackground(java.awt.Color.YELLOW);
+                            break;
+                        case 22:
+                            cells[i][j].setForeground(java.awt.Color.RED);
+                            cells[i][j].setBackground(java.awt.Color.YELLOW);
+                            break;
+                        case 23:
+                            cells[i][j].setForeground(java.awt.Color.BLUE);
+                            cells[i][j].setBackground(java.awt.Color.YELLOW);
+                            break;
+                        case 91:
+                            cells[i][j].setForeground(java.awt.Color.GRAY);
+                            break;
+                        case 92:
+                            cells[i][j].setForeground(java.awt.Color.RED);
+                            break;
+                        case 93:
+                            cells[i][j].setForeground(java.awt.Color.BLUE);
+                            break;
+                        default:
+                            cells[i][j].setForeground(java.awt.Color.BLACK);
+                            cells[i][j].setBackground(java.awt.Color.WHITE);
+                            break;
+                    }
                 }
+            }
+
+            for (int i = 0; i < data.length - board.length * board.length; i++) {
+                cells[i][N].setText(data[board.length * board.length + i]);
             }
 
         }
